bb2b33b6e6d98e571a6ef1da2da2b4ba00b748eb
ilzamafif <ilzamafif27@gmail.com>
2025-01-27 14:08:23 +0700

    add: authentication implemented

--- src/api/songs/routes.js
+++ src/api/songs/routes.js
@@ -1,28 +1,43 @@
 const routes = (handler) => [
   {
-    method: 'POST',
-    path: '/songs',
+    method: "POST",
+    path: "/songs",
     handler: handler.postSongHandler,
+    options: {
+      auth: "songsapp_jwt",
+    },
   },
   {
-    method: 'GET',
-    path: '/songs',
+    method: "GET",
+    path: "/songs",
     handler: handler.getSongsHandler,
+    options: {
+      auth: "songsapp_jwt",
+    },
   },
   {
-    method: 'GET',
-    path: '/songs/{id}',
+    method: "GET",
+    path: "/songs/{id}",
     handler: handler.getSongByIdHandler,
+    options: {
+      auth: "songsapp_jwt",
+    },
   },
   {
-    method: 'PUT',
-    path: '/songs/{id}',
+    method: "PUT",
+    path: "/songs/{id}",
     handler: handler.putSongByIdHandler,
+    options: {
+      auth: "songsapp_jwt",
+    },
   },
   {
-    method: 'DELETE',
-    path: '/songs/{id}',
+    method: "DELETE",
+    path: "/songs/{id}",
     handler: handler.deleteSongByIdHandler,
+    options: {
+      auth: "songsapp_jwt",
+    },
   },
 ];
 
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
+
 const songs = require('./api/songs');
 const SongsService = require('./services/postgres/SongsService');
 const SongsValidator = require('./validator/songs');
@@ -9,9 +11,16 @@ const albums = require('./api/albums');
 const AlbumsService = require('./services/postgres/AlbumsService');
 const AlbumsValidator = require('./validator/albums');
 
+// authentications
+const authentications = require("./api/authentications");
+const AuthenticationsService = require("./services/postgres/AuthenticationsService");
+const TokenManager = require("./tokenize/TokenManager");
+const AuthenticationsValidator = require("./validator/authentications");
+
 const init = async () => {
   const songsService = new SongsService();
   const albumsService = new AlbumsService();
+  const authenticationsService = new AuthenticationsService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -22,6 +31,31 @@ const init = async () => {
       },
     },
   });
+
+  // registrasi plugin eksternal
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  // mendefinisikan strategy autentikasi jwt
+  server.auth.strategy("songsapp_jwt", "jwt", {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
+  });
+
 
   await server.register([
     {
@@ -38,7 +72,33 @@ const init = async () => {
         validator: AlbumsValidator,
       },
     },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
+      },
+    },
   ]);
+
+  server.ext('onPreResponse', (request, h) => {
+    // mendapatkan konteks response dari request
+    const { response } = request;
+
+    // penanganan client error secara internal.
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    return h.continue;
+  });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
